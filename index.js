3217f8cafa41a45f14b030dd6472ad84256745b0
Amirthan Venkat <amirthanvenx@gmail.com>
2021-10-06 23:18:28 +0800

    gcloud config + express.js endpoints

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+app.get("/", async (req, res) => {
+  res.json({ status: "UP" });
+});
+
+app.get("/fx", (req, res, next) => {
+    rand1 = Math.floor(Math.random() * 90) + 10;
+    rand2 = Math.floor(Math.random() * 90) + 10;
+   
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+   
+    res.json({"sell": "489." + rand1, "timestamp": dateTime, "buy": "389." + rand2});
+   });
+
+app.listen(port, () => {
+  console.log(`App listening on port ${PORT}`);
+});
